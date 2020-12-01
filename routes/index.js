02d154faf970c737d4080c5597c6727257d11940
haidarrifki <19185336+haidarrifki@users.noreply.github.com>
2020-12-01 14:35:15 +0700

    Update not found message

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const userRoutes = require('./user');
 
 router.use(userRoutes);
 router.all('*', (req, res) => {
-  return res.status(404).json({ message: 'Hello, World.' });
+  return res.status(404).json({ message: 'No route and no API found with those values' });
 });
 
 module.exports = router;
